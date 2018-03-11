737b22d1d3627f68b9e59bcf3fa626a62e99951a
Thiago <souza.ty@gmail.com>
2018-03-10 21:52:27 -0300

    Update Functions

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 /**
 * @package WordPress
-* @subpackage HEELJ
-* @since HEELJ 1.0
+* @subpackage HURSO
+* @since HURSO 1.0
 */
 
 // Funtion load scripts (Carrega scripts do bootstrap e font Awesome)
@@ -19,7 +19,7 @@ function load_scripts() {
     add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
 // Add Functions
-require_once 'functions/heelj.php';
+require_once 'functions/custom-functions.php';
 require_once 'functions/menu.php';
 require_once 'functions/cpt.php';
 require_once 'functions/shortcodes.php';
